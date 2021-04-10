93562c00dabf56e0f149102067ce91f8f5065e85
Lucas Buzo <lucabtei@outlook.com>
2021-04-10 11:10:35 -0300

    menu

--- AGIOTA-CONVENIENCIA/src/app/cadVendas.java
+++ AGIOTA-CONVENIENCIA/src/app/cadVendas.java
@@ -1,18 +1,78 @@
 package app;
 
+import java.util.Scanner;
+
 import entities.Gelo;
 import entities.Produto;
 
-public class cadVendas {
-	public static void main(String[] args) {
-		Gelo gelo = new Gelo("gelo", "649", 15, 10);
+public class cadVendas  {
+	public static void main(String[] args) throws InterruptedException {
+		Scanner leia = new Scanner(System.in);
 		
-		gelo.retirarEstoque(4);
-		System.out.println(gelo.getQuantEstoque());
+		System.out.println("          ╔                             ╗");
+		System.out.print("          ║   Agiota Conveniência G7®️   ║\n");
+		System.out.print("	  ╚                             ╝\n");
+		System.out.print("──────────────────────────────────────────────────");
+		System.out.println("\n         Aqui, você não fica nos devendo...");
+		Thread.sleep(2000);
+		System.out.print("                   ( ಠ ʖ̯ ಠ )\n");
+		System.out.println();
+		System.out.println();
+			
+		Thread.sleep(2000);
 		
+		System.out.print("               Carregando sistema");
+
+		String reticencias = "";
+		
+		for (int x = 0; x < 3; x++) {			
+			for (int y = 0; y < 1; y++) {
+				reticencias = ".";
+				System.out.print(reticencias);
+				reticencias += ".";
+				Thread.sleep(2000);			
+			}
+		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("               [Sistema carregado]");
+		Thread.sleep(2000);
+		
+		limpatela();
+		
+		int tipoProduto;
+		
+		do {
+			System.out.print("\nDigite uma opção:\n\n1 - Bebidas\n2 - Salgadinhos\n3 - Doces\n4 - Gelo\n5 - Revistas\n6 - Sair\n");
+			tipoProduto = leia.nextInt();
+			
+			if (tipoProduto < 1) {
+				System.out.println("\nPor favor, insira uma opção válida");
+			} else if (tipoProduto == 1) {
+				System.out.println("\nOK");
+			} else if (tipoProduto == 2) {
+				
+			} else if (tipoProduto == 3) {
+				
+			} else if (tipoProduto == 4) {
+				
+			} else if (tipoProduto == 5) {
+				
+			} else {
+				
+			}
+			
+		} while (tipoProduto != 6);		
 	}
+
+	public static void limpatela() { 
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n"); 
+	}
+		
+}
 	
 
-}
+
 
 
